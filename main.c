d12155bdf11265f70388beb93bdff4787294481f
yuminchoi <chldbals219@naver.com>
2018-10-02 15:44:09 +0900

    second sub.

--- main.c
+++ main.c
@@ -5,21 +5,12 @@
 
 int main(int argc, char *argv[]) {
 	
-	int op1, op2;
-	int sum, sub;
+	int sec;
 	
-	printf("input two integers:");
-	scanf("%i %i", &op1, &op2);
+	printf("input the second:");
+	scanf("%d", &sec);
 	
-	sum=op1+op2;
-	sub=op1-op2;
-	
-
-    printf("+ operation: %i\n", sum);
-    printf("- operation: %i\n", sub);
-    printf("* operation: %i\n", op1*op2);
-    printf("/ operation: %i\n", op1/op2);
-    printf("%% operation: %i\n", op1%op2);
+	printf("%d sec. is %i : %i\n", sec, sec/60, sec%60);
 	
 	return 0;
 }
