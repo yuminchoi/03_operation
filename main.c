5d9c51ecf82290e591ce93cdfe2aa441be59010c
yuminchoi <chldbals219@naver.com>
2018-10-02 15:59:47 +0900

    third sub.

--- main.c
+++ main.c
@@ -5,12 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	int sec;
+	int year;
+	int result;
 	
-	printf("input the second:");
-	scanf("%d", &sec);
+	printf("input :");
+	scanf("%d", &year);
 	
-	printf("%d sec. is %i : %i\n", sec, sec/60, sec%60);
+	result= (year%4==0 && year%100!=0)||(year%400==0);
+	
+	printf("leap year? %d\n", result);
 	
 	return 0;
 }
